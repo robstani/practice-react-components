e3173700251356adeab9e9de8427260d226cb662
Robert Staniszewski <robstani.tech@gmail.com>
2024-05-23 10:31:07 +0200

    Finish task 01

--- 01/app.js
+++ 01/app.js
@@ -4,13 +4,31 @@ import { createRoot } from 'react-dom/client';
 const root = createRoot(document.querySelector('#root'));
 
 class App extends React.Component {
-    state = {
-        counter: 0,
+    constructor(props) {
+        super(props);
+        this.state = { counter: 0 };
+        this.intervalId = null;
     }
     render() {
         console.log('render');
 
         return <h1>{ this.state.counter }</h1>
+    }
+
+    componentDidMount() {
+        this.intervalId = setInterval(() => {
+            this.setState((prevState) => ({ counter: prevState.counter + 1 }));
+        }, 5000);
+        console.log('component mount')
+    }
+
+    componentDidUpdate() {
+        console.log('component update')
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+        console.log('component unmount')
     }
 }
 
